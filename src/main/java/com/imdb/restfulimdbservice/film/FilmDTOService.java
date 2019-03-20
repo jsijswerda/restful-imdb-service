b6b834d76ebb0fa92c50bea1699f8d83f337b3f4
Jasper Sijswerda <jsijswerda@sogyo.nl>
2019-03-20 12:06:59 +0100

    get mapping added - works now

--- src/main/java/com/imdb/restfulimdbservice/film/FilmDTOService.java
+++ src/main/java/com/imdb/restfulimdbservice/film/FilmDTOService.java
@@ -1,18 +1,22 @@
 package com.imdb.restfulimdbservice.film;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.imdb.restfulimdbservice.film.Film;
 
-@Component
+@Service
 public class FilmDTOService {
 
-	private static List<Film> films;
+	private static List<Film> films = new ArrayList<Film>();
 	
-	
+	static {
+		films.add(new Film(1,"Green Book", Arrays.asList(new Person("Viggo", "Mortesen")),new Person("Peter", "Farelly"), new Date()));
+	}
 	
 	public List<Film> getAllFilms(){
 		return films;
--- src/main/java/com/imdb/restfulimdbservice/welcome/Welcome.java
+++ src/main/java/com/imdb/restfulimdbservice/welcome/Welcome.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Welcome {
 
-	@GetMapping(path="/welkom")
+	@GetMapping(path="/welcome")
 	public String welcome() {
 		return "welcome";
 	}
